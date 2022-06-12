5a53c695a4c32d3d58b38f10dd38982df594c101
Steven Laszloffy <steven.laszloffy@gmail.com>
2022-06-12 01:30:28 +0000

    oops, commiting finished lab exercises

--- lab_dictionaries.py
+++ lab_dictionaries.py
@@ -1,8 +1,13 @@
 # 1) Set the emails variable to be an empty dictionary
+emails = {}
 
 assert emails == {}, f"Expected `emails` to be {{}} but got: {repr(emails)}"
 
 # 2) Add 'ashley', 'craig', and 'elizabeth' to the emails dictionary without reassigning the variable.
+emails['ashley'] = 'ashley@example.com'
+emails['craig'] = 'craig@example.com'
+emails['elizabeth'] = 'elizabeth@example.com'
+
 
 assert emails == {
     "ashley": "ashley@example.com",
@@ -11,6 +16,7 @@ assert emails == {
 }, f"Expected `emails` to be {{'ashley': 'ashley@example.com', 'craig': 'craig@example.com', 'elizabeth': 'elizabeth@example.com'}} but got: {repr(emails)}"
 
 # 3) Remove 'craig' from the emails dictionary without reassigning the variable.
+del emails['craig']
 
 assert emails == {
     "ashley": "ashley@example.com",
@@ -18,6 +24,7 @@ assert emails == {
 }, f"Expected `emails` to be {{'ashley': 'ashley@example.com', 'elizabeth': 'elizabeth@example.com'}} but got: {repr(emails)}"
 
 # 4) Add 'dalton' to the emails dictionary without reassigning the variable.
+emails['dalton'] = 'dalton@example.com'
 
 assert emails == {
     "ashley": "ashley@example.com",
@@ -26,6 +33,7 @@ assert emails == {
 }, f"Expected `emails` to be {{'ashley': 'ashley@example.com', 'elizabeth': 'elizabeth@example.com', 'dalton': 'dalton@example.com'}} but got: {repr(emails)}"
 
 # 5) Return a list of keys from the emails dictionary as `users`
+users = list(emails.keys())
 
 assert users == [
     "ashley",
@@ -34,6 +42,7 @@ assert users == [
 ], f"Expected `users` to be ['ashley', 'elizabeth', 'dalton'] but got: {repr(users)}"
 
 # 6) Return a list of values from the emails dictionary as `email_list`
+email_list = list(emails.values())
 
 assert email_list == [
     "ashley@example.com",
@@ -42,6 +51,7 @@ assert email_list == [
 ], f"Expected `email_list` to be ['ashely@example.com', 'elizabeth@example.com', 'dalton@example.com'] but got: {repr(email_list)}"
 
 # 7) Return a list of tuples called `pairs` representing the key/value pairs in `emails`.
+pairs = list(emails.items())
 
 assert pairs == [
     ("ashley", "ashley@example.com"),
