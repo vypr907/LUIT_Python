001a89e629b29e9862176979852a4324f7f155ed
Steven Laszloffy <steven.laszloffy@gmail.com>
2022-06-12 02:13:11 +0000

    completed steps 4 and 5

--- wk14Project-TheListerator.py
+++ wk14Project-TheListerator.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env Python3.7
+#!/usr/bin/env python3.7
 
 # --------------------------------------------------------------
 # Program: The Listerator
@@ -16,7 +16,7 @@
 services = []
 
 #populate the list from text file
-file = open('LUIT_Python/services.txt','r')
+file = open('services.txt','r')
 for line in file:
     strippedLine = line.strip()
     services.append(strippedLine)
@@ -27,3 +27,20 @@ print(services)
 #print the length
 print("The list has",len(services),"entries.")
 
+#remove two services from the list by name or by index
+#for giggles, letting the user pick via number.. and for time, 
+# we are assuming they won't try and break the fragile code and my heart...
+print("AWS is downsizing it's services, and you get to pick which two get the axe!")
+print("Please enter a number between 1 and",len(services),":",end = ' ')
+choiceOne = int(input())
+choiceTwo = int(input("Please enter your second pick: "))
+#adjusting to index
+choiceOne = choiceOne - 1
+choiceTwo = choiceTwo - 1
+print("Great picks! Say goodbye to", services[choiceOne], "and", services[choiceTwo],":)")
+#deleting the chosen values
+del services[choiceOne]
+del services[choiceTwo-1]
+
+#print the new list and the new length
+print("The new list is: ",services, "and the length is",len(services))
