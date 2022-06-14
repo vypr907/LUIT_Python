29418edeb3e3e7895f19caa322a63f8b9ea83ad4
Steven Laszloffy <steven.laszloffy@gmail.com>
2022-06-14 19:48:57 +0000

    creating file for project

---
+++ wk15Project-TheNamerator.py
@@ -0,0 +1,12 @@
+#!/user/bin/env python3.7
+
+# --------------------------------------------------------------
+# Program: The Namerator
+# Programmer: Steven Laszloffy
+# Project: LUIT Week 15
+# Purpose: A simple program that generates EC2 names based on departments.
+# - allow the user to input how many names they need
+# - allow the user to input their department name to be used in the EC2 name
+# - generate random characters and numbers that will be included in the name
+# ---------------------------------------------------------------
+
