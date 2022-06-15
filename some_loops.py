d074de3648d81936ac5e41c75cf7799f1bcec73e
Steven Laszloffy <steven.laszloffy@gmail.com>
2022-06-15 00:13:01 +0000

    some more tuesday meeting code play

---
+++ some_loops.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.7
+
+import random
+
+numbers = [0,1,2,3,4,5]
+
+#print(numbers)
+
+for i in range(10):
+    #print(i)
+    pass
+    
+
+counter = 0
+while(counter < 5):
+    print(random.randint(0,5))
+    counter += 1
