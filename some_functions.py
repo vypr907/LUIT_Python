f77c60f214a85bb405616a8fcee3bbf4adae7fc9
Steven Laszloffy <steven.laszloffy@gmail.com>
2022-06-16 14:53:34 +0000

    coursework code doodles

---
+++ some_functions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.7
+
+def print_name(name):
+    print(f"Name is {name}")
+    
+def add_two(num):
+    return num + 2
+    
+    
+aux = ""
+i = 0
+
+#test for print_name
+#while i < 10:
+#    aux = input("Enter a name: ")
+#    print_name(aux)
+
+
