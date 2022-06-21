72817d513695bed6ab8647ef7c37ea12023816cd
Steven Laszloffy <steven.laszloffy@gmail.com>
2022-06-21 20:48:48 +0000

    added another way to format the returned values

--- bucketCreationDate.py
+++ bucketCreationDate.py
@@ -4,8 +4,13 @@ s3_resource = boto3.client("s3")
 
 s3_resource.list_buckets()["Buckets"][0]["Name"]
 
+
+
 print("___________________")         #    [item][list index][attribute]
 creation_date = s3_resource.list_buckets()["Buckets"][0]["CreationDate"]
+print(creation_date)
+print("____________________")
+creation_date.strftime("%d%m%y_%H:%M:%s") #alternate way to format the contents
 print(creation_date)
 print("____________________")
 
